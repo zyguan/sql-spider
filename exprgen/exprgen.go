13b574bbdef3093c8710dd71e71ae9166d1bccf2
zyguan <zhongyangguan@gmail.com>
2019-10-26 13:32:24 +0800

    impl fillTable

--- exprgen/exprgen.go
+++ exprgen/exprgen.go
@@ -1,10 +1,14 @@
 package exprgen
 
-import "github.com/zyguan/sqlgen/util"
+import (
+	"math/rand"
+
+	"github.com/zyguan/sqlgen/util"
+)
 
 func GenExprTrees(tree util.Tree, ts util.TableSchemas, n int) []util.Tree {
 	trees := make([]util.Tree, 0, n)
-	for i := 0; i < n; i ++ {
+	for i := 0; i < n; i++ {
 		t := tree.Clone()
 		fillTree(t)
 		trees = append(trees, t)
@@ -32,12 +36,24 @@ func fillNode(node util.Node, ts util.TableSchemas) {
 }
 
 func fillTable(t *util.Table, ts util.TableSchemas) {
-	// TODO
+	t.SelectedColumns = make([]string, 0, len(t.Schema.Columns()))
+	for _, col := range t.Schema.Columns() {
+		if rand.Float64() < .5 {
+			t.SelectedColumns = append(t.SelectedColumns, col.ToSQL())
+		}
+	}
 }
 
 func fillProj(p *util.Projector) {
 	nCols := p.Children()[0].NumCols()
-	// TODO
+	nProjected := rand.Intn(nCols * 2)
+	if nProjected == 0 {
+		nProjected = 1
+	}
+	p.Projections = make([]util.Expr, nProjected)
+	for i := 0; i < nProjected; i++ {
+		p.Projections[i] = buildExpr(nCols)
+	}
 }
 
 func fillJoin(j *util.Join) [][]util.Expr {
@@ -50,4 +66,8 @@ func fillFilter(f *util.Filter) []util.Expr {
 	nCols := f.Children()[0].NumCols()
 	// TODO
 	return nil
+}
+
+func buildExpr(nCols int) util.Expr {
+	// TODO
 }
