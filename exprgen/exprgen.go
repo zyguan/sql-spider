e574679794dc40e1c96aab82dab110f7f2d282ac
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2019-10-26 12:01:54 +0800

    fixup

--- exprgen/exprgen.go
+++ exprgen/exprgen.go
@@ -5,3 +5,15 @@ import "github.com/zyguan/sqlgen/util"
 func GenExprTrees(tree util.Tree, ts util.TableSchemas) []util.Tree {
 	return nil
 }
+
+func genFilter(numCols []int) []util.Expr {
+	return nil
+}
+
+func genProj(numCols []int) [][]util.Expr {
+	return nil
+}
+
+func genJoinCond(numLCols, numRCols int) []util.Expr {
+	return nil
+}
